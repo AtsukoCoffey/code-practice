d112b9f087ecd008763893a881b158304075b16a
Atsuko Coffey <atsucoffey@gmail.com>
2024-05-22 23:59:06 +0000

    fix: fix the letter counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,131 +124,95 @@ const words = [
 
 
 
-// Shuffle the array object
-let shuffledWords = [];
 
-function shuffle() {
-  for (word of words) {
-    shuffledWords.push(words[Math.floor(Math.random() * 80)]);
-  }
-}
-
-
-
-// Wait for the DOM to finish loading before running the game
-// Start the global time-limit and start game
-document.addEventListener("DOMContentLoaded", function () {
-  shuffle();
-  startGame();
-  setTimeout(function () {
-    finishGame();
-  }, 60000);
-  // keyboard event
-  // window.addEventListener('keypress', handleKeyPress);
-  // Enter key -> validate input function for mobile screen
-  let input = document.getElementById('input');
-  input.addEventListener('keypress', handleKeyPress)
-  // input.addEventListener('keypress', function (event) {
-
-
-  // }); // Enter key action
-
-}); //Add event listener
-
-
+/**
+ * All the global counter and variables
+ *///////////////////////////////////////////////////////////////
+// Get div for display questions
+let imageDisplay = document.getElementById('img-display');
+let textDisplay = document.getElementById('text-display');
+let kanaDisplay = document.getElementById('kana-display');
+let textOver = document.getElementById('text-overlay');
+// let textOver = document.getElementById('text-overlay');
+let input = document.getElementById('input'); // input box
 
 // Game counter = Number of correct answer
-let g = 0;
-// Letter counter : correct / mistake
-let c = 0;
-let m = 0;
-// Success rate : correct / correct + mistake * 100
-let successRate = Math.floor(( c / (c + m) )* 100); 
+let gameCounter = 0;
+// Letter counter 
+let letterCounter = 0;
+// let kanaLetterCounter = 0;
+// Correct / Mistake Letter counter
+let correctTypeCounter = 0;
+let missTypeCounter = 0;
 
+
+/**
+ * All the main functions
+ *///////////////////////////////////////////////////////////////
+// Shuffle the array object
+// let shuffledWords = [];
+// function shuffle() {
+//   for (word of words) {
+//     shuffledWords.push(words[Math.floor(Math.random() * 40)]);
+//   }
+// }
 
 /**
  * Start game function
  * loop through the shuffled question
 */
-
-
 function startGame() {
-  // Get div for display questions
-  let imageDisplay = document.getElementById('img-display');
-  let textDisplay = document.getElementById('text-display');
-  let kanaDisplay = document.getElementById('kana-display');
-  // let textOver = document.getElementById('text-overlay');
-
-  imageDisplay.innerHTML = '<img src="' + shuffledWords[g].image + '" width="100" height="100" alt="Question word">';
-
-  kanaDisplay.innerHTML = shuffledWords[g].kana;
-  textDisplay.innerHTML = shuffledWords[g].name;
-
+  // Display next (shuffled) words word
+  imageDisplay.innerHTML = '<img src="' + words[gameCounter].image + '" width="100" height="100" alt="Question word">';
+  kanaDisplay.innerHTML = words[gameCounter].kana;
+  textDisplay.innerHTML = words[gameCounter].name;
 
   // Reset the div with empty value
-  document.getElementById('text-overlay').innerHTML = "";
+  document.getElementById('text-overlay').textContent = "";
   document.getElementById('input').value = "";
 
   // Set one word timer and restart
-  // setTimeout(function () {
-  //   startGame();
-  // }, 10000);
+  setTimeout(function () {
+    document.getElementById('hit-sound').play();
+    gameCounter++;
+    letterCounter = 0;
+    startGame();
+  }, 20000);
 }
-
-/**
- * Finish game function
- */
-function finishGame() {
-  let textDisplay = document.getElementById('text-display');
-  let textOver = document.getElementById('text-overlay');
-
-  textDisplay.innerHTML = "Time out";
-  textOver.innerHTML = "Time out";
-
-      // typing sound
-      document.getElementById('ending-sound').play();
-  
-
-  document.getElementsByTagName('main')[0].innerHTML =
-    `<div><p>Menu</p><ul><li>Sushi menu</li><li>Travel in Japan</li><li>Greetings</li></ul></div><div><p>Score</p><ul><li>Clear : ${g}</li><li>Miss : 0</li><li>Success rate : ${successRate}%</li></ul></div>`;
-}
-
-
-
-// Letter counter ----------------------------------------------------
-let i = 0;
-let a = 0;
 
 /**
  * Determine pressed key, change color, add counter to go next letter
  */
-
 function handleKeyPress(event) {
   // Access the key that was pressed
   const key = event.key;
   // let kana = detectBoin(key);
 
-  let textDisplay = document.getElementById('text-display').innerText;
-  // let textOver = document.getElementById('text-overlay').innerText;
-  // let kanaDisplay = document.getElementById('kana-display').innerText;
-
   // Check whether match the letter
-  if (key === textDisplay.charAt(i)) {
-    let currentLetter = textDisplay.charAt(i);
-
-    // Add matched letter to the overlay div
-    let textOver = document.getElementById('text-overlay').innerText;
-    textOver += currentLetter;
-
-    // Update the div
-    document.getElementById('text-overlay').innerHTML = textOver;
-
+  if (key === textDisplay.innerText.charAt(letterCounter)) {
     // typing sound
     document.getElementById('type-sound').play();
 
+    let currentLetter = textDisplay.innerText.charAt(letterCounter);
+
+    // Add matched letter to the overlay div
+    // textOver.innerText += currentLetter;
+
+    // Update the div
+    document.getElementById('text-overlay').innerHTML = textOver.innerText += currentLetter;
+    
     // Go to next word
-    i++;
-    c++;
+    letterCounter++;
+    correctTypeCounter++;
+
+    // Reset input box
+    document.getElementById('input').value = "";
+
+console.log('Letter Counter is: ', letterCounter);
+
+    // Enter key action
+  } else if (key === "Enter") {
+    validateInput();
 
     // Check user input is correct for mobile user
   } else {
@@ -256,37 +220,78 @@ function handleKeyPress(event) {
     document.getElementById('hit-sound').play();
 
     // Mistake counter
-    m++;
-  }
+    missTypeCounter++;
 
-
-  if (key === "Enter") {
-    validateInput();
+    // Reset input box
+    document.getElementById('input').value = "";
   }
   // console the pressed key
   console.log('Key pressed: ' + key);
 
 }
 
-
-
 /**
-   * Validate user entry when correct typing, go on to next
-   */
+  * Validate user entry when correct typing, go on to next
+  *///   // Enter key action
 function validateInput() {
-  let textDisplay = document.getElementById('text-display').innerText;
-  let textOver = document.getElementById('text-overlay').innerText;
-  let input = document.getElementById('input').innerText;
-  if (textOver === textDisplay || input === textDisplay) {
-    //counter reset
-    i = 0;
-    a = 0;
-    g++;
+  // let lastLetter = words[gameCounter].length;
+
+  if (textOver.innerText === textDisplay.innerText || input.innerText === textDisplay.innerText) {
+
+    // letter counter reset
+    letterCounter = 0;
+    // kanaLetterCounter = 0;
+    gameCounter++;
     // Next word
     startGame();
 
   }
 }
+
+/**
+ * Finish game function
+ */
+function finishGame() {
+  textDisplay.innerHTML = "Time out";
+  textOver.innerHTML = "Time out";
+  
+
+  // Ending sound
+  document.getElementById('ending-sound').play();
+
+  // Score display
+  document.getElementsByTagName('main')[0].innerHTML =
+    `<div><p>Menu</p><ul><li>Sushi menu</li><li>Travel in Japan</li><li>Greetings</li></ul></div><div><p>Score</p><ul><li>Clear : ${gameCounter}</li><li>Miss : 0</li><li>Success rate : ${successRate}%</li></ul></div>`;
+}
+
+
+
+// keyboard window event
+// window.addEventListener('keypress', handleKeyPress);
+
+// Add event to input box -> validate and enter button action
+input.addEventListener('keypress', handleKeyPress);
+
+
+
+// Wait for the DOM to finish loading before running the game
+// Start the global time-limit and start game
+document.addEventListener("DOMContentLoaded", function () {
+  // shuffle();
+  startGame();
+  setTimeout(function () {
+    finishGame();
+  }, 60000);
+}); //Add event listener
+
+
+/**
+ * Calculate the score
+ */
+// Success rate : correct / correct + mistake * 100
+let successRate = Math.floor((correctTypeCounter / (correctTypeCounter + missTypeCounter)) * 100);
+
+
 
 
 
