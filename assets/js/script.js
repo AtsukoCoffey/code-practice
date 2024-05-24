90007044e8b4c97bfc182ed69912ff0d866adbe8
Atsuko Coffey <atsucoffey@gmail.com>
2024-05-24 19:39:24 +0000

    test: Test site for PP2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,158 @@
-// Question array object
+////////////////////////////////////////////// Question array object
 const words = [
   {
     name: 'amaebi',
     "abc": 'amaebi',
     "kana": 'あまえび',
-    image: 'assets/images/sushi_amaebi.png'
+    image: 'assets/images/sushi_amaebi.webp'
   },
   {
     name: 'anago',
     "abc": 'anago',
     "kana": 'あなご',
-    image: 'assets/images/sushi_anago.png'
+    image: 'assets/images/sushi_anago.webp'
   },
   {
     name: 'battera',
     "abc": 'battera',
     "kana": 'ばってら',
-    image: 'assets/images/sushi_battera.png'
+    image: 'assets/images/sushi_battera.webp'
   },
   {
     name: 'buri',
     "abc": 'buri',
     "kana": 'ぶり',
-    image: 'assets/images/sushi_buri.png'
+    image: 'assets/images/sushi_buri.webp'
   },
   {
     name: 'kappa',
     "abc": 'buri',
     "kana": 'かっぱ',
-    image: 'assets/images/makimono_kappa.png'
+    image: 'assets/images/makimono_kappa.webp'
   },
   {
     name: 'negitoro',
     "abc": 'buri',
     "kana": 'ねぎとろ',
-    image: 'assets/images/makimono_negitoro.png'
+    image: 'assets/images/makimono_negitoro.webp'
   },
   {
     name: 'takuwan',
     "abc": 'buri',
     "kana": 'たくあん',
-    image: 'assets/images/makimono_takuwan.png'
+    image: 'assets/images/makimono_takuwan.webp'
   },
   {
     name: 'chutoro',
     "abc": 'buri',
     "kana": 'ちゅうとろ',
-    image: 'assets/images/sushi_chutoro.png'
+    image: 'assets/images/sushi_chutoro.webp'
   },
   {
     name: 'ebi',
     "abc": 'buri',
     "kana": 'えび',
-    image: 'assets/images/sushi_ebi.png'
+    image: 'assets/images/sushi_ebi.webp'
   },
   {
     name: 'hamachi',
     "abc": 'buri',
     "kana": 'はまち',
-    image: 'assets/images/sushi_hamachi.png'
+    image: 'assets/images/sushi_hamachi.webp'
   },
   {
     name: 'hanba-gu',
     "abc": 'buri',
     "kana": 'はんばーぐ',
-    image: 'assets/images/sushi_hamburg_tare.png'
+    image: 'assets/images/sushi_hamburg_tare.webp'
   },
   {
     name: 'hotate',
     "abc": 'buri',
     "kana": 'ほたて',
-    image: 'assets/images/sushi_hotate.png'
+    image: 'assets/images/sushi_hotate.webp'
   },
   {
     name: 'ika',
     "abc": 'buri',
     "kana": 'いか',
-    image: 'assets/images/sushi_ika.png'
+    image: 'assets/images/sushi_ika.webp'
   },
   {
     name: 'ikura',
     "abc": 'buri',
     "kana": 'いくら',
-    image: 'assets/images/sushi_ikura.png'
+    image: 'assets/images/sushi_ikura.webp'
   },
   {
     name: 'katsuo',
     "abc": 'buri',
     "kana": 'かつお',
-    image: 'assets/images/sushi_katsuo.png'
+    image: 'assets/images/sushi_katsuo.webp'
   },
   {
     name: 'sake',
     "abc": 'buri',
     "kana": 'さけ',
-    image: 'assets/images/sushi_salmon.png'
+    image: 'assets/images/sushi_salmon.webp'
   },
   {
     name: 'tai',
     "abc": 'buri',
     "kana": 'たい',
-    image: 'assets/images/sushi_tai.png'
+    image: 'assets/images/sushi_tai.webp'
   },
   {
     name: 'tako',
     "abc": 'buri',
     "kana": 'たこ',
-    image: 'assets/images/sushi_tako.png'
+    image: 'assets/images/sushi_tako.webp'
   },
   {
     name: 'tamago',
     "abc": 'buri',
     "kana": 'たまご',
-    image: 'assets/images/sushi_tamago.png'
+    image: 'assets/images/sushi_tamago.webp'
   },
   {
     name: 'inarizushi',
     "abc": 'buri',
     "kana": 'いなりずし',
-    image: 'assets/images/food_inarizushi_set.png'
+    image: 'assets/images/food_inarizushi_set.webp'
   },
 ];
 
-
-
-
 /**
- * All the global counter and variables
- *///////////////////////////////////////////////////////////////
+ *  Shuffle the question array object 
+ *  Using Fisher Yates shuffle method : [BUG] article from stackoverflow
+ *  */
+// Create a shalow copy of words array
+let shuffledWords = [...words];
+// for loop from last index
+for (i = shuffledWords.length - 1; i > 0; i--) {
+  randomIndexOfWords = Math.floor(Math.random() * i)
+  // escape value of last index of shuffledWords array
+  temporaryValue = shuffledWords[i]
+  // swap last index value to random value
+  shuffledWords[i] = shuffledWords[randomIndexOfWords]
+  // swap random index value to last index valu
+  shuffledWords[randomIndexOfWords] = temporaryValue
+}
+
+
+
+
+///////////////////////////////////////////   All the global counter and variables
+
 // Get div for display questions
 let imageDisplay = document.getElementById('img-display');
 let textDisplay = document.getElementById('text-display');
-let kanaDisplay = document.getElementById('kana-display');
 let textOver = document.getElementById('text-overlay');
-// let textOver = document.getElementById('text-overlay');
+let kanaDisplay = document.getElementById('kana-display');
+let kanaOver = document.getElementById('kana-overlay');
 let input = document.getElementById('input'); // input box
 
-// Game counter = Number of correct answer
+// Game counter  
 let gameCounter = 0;
 // Letter counter 
 let letterCounter = 0;
@@ -146,75 +162,64 @@ let correctTypeCounter = 0;
 let missTypeCounter = 0;
 
 
-/**
- * All the main functions
- *///////////////////////////////////////////////////////////////
-// Shuffle the array object
-// let shuffledWords = [];
-// function shuffle() {
-//   for (word of words) {
-//     shuffledWords.push(words[Math.floor(Math.random() * 40)]);
-//   }
-// }
+///////////////////////////////////////////   All the main functions
 
 /**
  * Start game function
  * loop through the shuffled question
-*/
+ */
 function startGame() {
-  // Display next (shuffled) words word
-  imageDisplay.innerHTML = '<img src="' + words[gameCounter].image + '" width="100" height="100" alt="Question word">';
-  kanaDisplay.innerHTML = words[gameCounter].kana;
-  textDisplay.innerHTML = words[gameCounter].name;
+  // Display next shuffled word
+  imageDisplay.innerHTML = '<img src="' + shuffledWords[gameCounter].image + '" width="100" height="100" alt="Question word">';
+  kanaDisplay.innerHTML = shuffledWords[gameCounter].kana;
+  textDisplay.innerHTML = shuffledWords[gameCounter].name;
 
   // Reset the div with empty value
   document.getElementById('text-overlay').textContent = "";
+  document.getElementById('kana-overlay').textContent = "";
   document.getElementById('input').value = "";
 
-  // Set one word timer and restart
-  setTimeout(function () {
-    document.getElementById('hit-sound').play();
-    gameCounter++;
-    letterCounter = 0;
-    startGame();
-  }, 20000);
+  // // Set one word timer and restart
+  // setTimeout(function () {
+  //   document.getElementById('hit-sound').play();
+  //   gameCounter++;
+  //   letterCounter = 0;
+  //   startGame();
+  // }, 20000);
+
 }
 
 /**
- * Determine pressed key, change color, add counter to go next letter
+ * Determine pressed key function
+ * if matched change color, add counter to go next letter
+ * NOT work for mobile screen keyboard --> validate function
  */
 function handleKeyPress(event) {
+
   // Access the key that was pressed
   const key = event.key;
-  // let kana = detectBoin(key);
 
   // Check whether match the letter
-  if (key === textDisplay.innerText.charAt(letterCounter)) {
+  if (key == textDisplay.textContent.charAt(letterCounter)) {
     // typing sound
     document.getElementById('type-sound').play();
 
     let currentLetter = textDisplay.innerText.charAt(letterCounter);
 
-    // Add matched letter to the overlay div
-    // textOver.innerText += currentLetter;
-
-    // Update the div
+    // Add matched letter to the overlay div and update the div
     document.getElementById('text-overlay').innerHTML = textOver.innerText += currentLetter;
-    
+
     // Go to next word
     letterCounter++;
     correctTypeCounter++;
 
-    // Reset input box
-    document.getElementById('input').value = "";
+    console.log('Letter Counter is: ', letterCounter);
 
-console.log('Letter Counter is: ', letterCounter);
+    // Enter key action  // Check user input is correct for mobile user
 
-    // Enter key action
   } else if (key === "Enter") {
     validateInput();
 
-    // Check user input is correct for mobile user
   } else {
     // wrong typing sound
     document.getElementById('hit-sound').play();
@@ -222,9 +227,11 @@ console.log('Letter Counter is: ', letterCounter);
     // Mistake counter
     missTypeCounter++;
 
-    // Reset input box
-    document.getElementById('input').value = "";
   }
+
+  // Reset input box
+  document.getElementById('input').value = "";
+
   // console the pressed key
   console.log('Key pressed: ' + key);
 
@@ -234,9 +241,11 @@ console.log('Letter Counter is: ', letterCounter);
   * Validate user entry when correct typing, go on to next
   *///   // Enter key action
 function validateInput() {
-  // let lastLetter = words[gameCounter].length;
 
-  if (textOver.innerText === textDisplay.innerText || input.innerText === textDisplay.innerText) {
+  // // submit from form
+  // event.preventDefault();
+
+  if (textOver.innerText == textDisplay.innerText || input.innerText == textDisplay.innerText) {
 
     // letter counter reset
     letterCounter = 0;
@@ -254,25 +263,23 @@ function validateInput() {
 function finishGame() {
   textDisplay.innerHTML = "Time out";
   textOver.innerHTML = "Time out";
-  
 
   // Ending sound
   document.getElementById('ending-sound').play();
 
   // Score display
   document.getElementsByTagName('main')[0].innerHTML =
-    `<div><p>Menu</p><ul><li>Sushi menu</li><li>Travel in Japan</li><li>Greetings</li></ul></div><div><p>Score</p><ul><li>Clear : ${gameCounter}</li><li>Miss : 0</li><li>Success rate : ${successRate}%</li></ul></div>`;
+    `<div><p>Menu</p><ul><li>Sushi menu</li><li>Travel in Japan</li><li>Greetings</li></ul></div><div><p>Score</p><ul><li>Clear : ${gameCounter}</li><li>Miss : 0</li><li>Success rate : %</li></ul></div>`;
 }
 
 
-
 // keyboard window event
-// window.addEventListener('keypress', handleKeyPress);
+window.addEventListener('keypress', handleKeyPress);
+// Submit for validate input - mobile device
+input.addEventListener('input', validateInput);
 
-// Add event to input box -> validate and enter button action
-input.addEventListener('keypress', handleKeyPress);
-
-
+// // Add event to input box -> validate and enter button action
+// input.addEventListener('keypress', handleKeyPress);
 
 // Wait for the DOM to finish loading before running the game
 // Start the global time-limit and start game
@@ -285,89 +292,143 @@ document.addEventListener("DOMContentLoaded", function () {
 }); //Add event listener
 
 
+
 /**
- * Calculate the score
+ * Difine kana alphabet
+ * I will challenge this matching system if I had a time
  */
-// Success rate : correct / correct + mistake * 100
-let successRate = Math.floor((correctTypeCounter / (correctTypeCounter + missTypeCounter)) * 100);
+// let lastIndex = document.getElementById('text-overlay').innerText.length;
+// let lastSecondIndex = document.getElementById('text-overlay').innerText.length - 1;
+// let lastThirdIndex = document.getElementById('text-overlay').innerText.length - 2;
+// let kanaLasT = textOver.textContent.charAt(lastThirdIndex);
+// let kanaLasS = textOver.textContent.charAt(lastSecondIndex);
+// let kanaLast = document.getElementById('text-overlay').innerText;
+// let kana;
+// kana = detectBoin();
+// let hiragana;
+// if (kana) {
+//   hiragana = kana;
+// }
+// // Add matched letter to the overlay div and update the div
+// document.getElementById('kana-overlay').innerHTML = kanaOver.innerText += hiragana;
+// kana = "";
+// hiragana = "";
+/**
+ * Defining kana using textOver's input data
+ */
+// function detectBoin() {
+//   if (!lastSecondIndex || !lastThirdIndex) {
+//     if (kanaLast === 'a') {
+//       return "あ";
+//     } else if (kanaLast === 'i') {
+//       return "い";
+//     } else if (kanaLast === 'u') {
+//       return "う";
+//     } else if (kanaLast === 'e') {
+//       return "え";
+//     } else if (kanaLast === 'o') {
+//       return "お";
+//     }
+//   } else if (lastSecondIndex || lastThirdIndex) {
+//     if (kanaLasS === 'm' && kanaLast === 'a') {
+//       return "ま";
+//     } else if (kanaLasS === 'b' && kanaLast === 'i') {
+//       return "び";
+//     }
+//   }
+// };
+// else if (k === 'k' && kanaLast === 'a') {
+//   return "か";
+// } else if (k === 'k' && kanaLast === 'i') {
+//   return "き";
+// } else if (k === 'k' && kanaLast === 'u') {
+//   return "く";
+// } else if (k === 'k' && kanaLast === 'e') {
+//   return "け";
+// } else if (k === 'k' && kanaLast === 'o') {
+//   return "こ";
+// } else if (k === 's' && kanaLast === 'a') {
+//   return "さ";
+// } else if ((k === 's' && kanaLast === 'i') || (k === 's' && a === 'h' && kanaLast === 'i')) {
+//   return "し";
+// } else if (k === 's' && kanaLast === 'u') {
+//   return "す";
+// } else if (k === 's' && kanaLast === 'e') {
+//   return "せ";
+// } else if (k === 's' && kanaLast === 'e') {
+//   return "そ";
+// } else if (k === 't' && kanaLast === 'a') {
+//   return "た";
+// } else if ((k === 't' && kanaLast === 'u') || (k === 't' && a === 's' && kanaLast === 'u')) {
+//   return "ち";
+// } else if ((k === 't' && kanaLast === 'u') || (k === 't' && a === 's' && kanaLast === 'u')) {
+//   return "つ";
+// } else if (k === 't' && kanaLast === 'e') {
+//   return "て";
+// } else if (k === 't' && kanaLast === 'o') {
+//   return "と";
+// } else if (k === 'n' && kanaLast === 'a') {
+//   return "な";
+// } else if (k === 'n' && kanaLast === 'i') {
+//   return "に";
+// } else if (k === 'n' && kanaLast === 'u') {
+//   return "ぬ";
+// } else if (k === 'n' && kanaLast === 'e') {
+//   return "ね";
+// } else if (k === 'n' && kanaLast === 'o') {
+//   return "の";
+// } else if (k === 'h' && kanaLast === 'a') {
+//   return "は";
+// } else if (k === 'h' && kanaLast === 'i') {
+//   return "ひ";
+// } else if (k === 'f' && kanaLast === 'u') {
+//   return "ふ";
+// } else if (k === 'h' && kanaLast === 'e') {
+//   return "へ";
+// } else if (k === 'h' && kanaLast === 'o') {
+//   return "ほ";
+// } else if (k === 'm' && kanaLast === 'a') {
+//   return "ま";
+// } else if (k === 'm' && kanaLast === 'i') {
+//   return "み";
+// } else if (k === 'm' && kanaLast === 'u') {
+//   return "む";
+// } else if (k === 'm' && kanaLast === 'e') {
+//   return "め";
+// } else if (k === 'm' && kanaLast === 'o') {
+//   return "も";
+// } else if (k === 'y' && a === 'a') {
+//   return "や";
+// } else if (k === 'y' && a === 'u') {
+//   return "ゆ";
+// } else if (k === 'y' && a === 'o') {
+//   return "よ";
+// } else if (k === 'r' && a === 'a') {
+//   return "ら";
+// } else if (k === 'r' && a === 'i') {
+//   return "り";
+// } else if (k === 'r' && a === 'u') {
+//   return "る";
+// } else if (k === 'r' && a === 'e') {
+//   return "れ";
+// } else if (k === 'r' && a === 'o') {
+//   return "ろ";
+// } else if (k === 'w' && a === 'a') {
+//   return "わ";
+// } else if (k === 'n' && a === 'n') {
+//   return "ん";
+// } else if (a === 'a') {
+//   return "あ";
+// } else if (a === 'i') {
+//   return "い";
+// } else if (a === 'u') {
+//   return "う";
+// } else if (a === 'e') {
+//   return "え";
+// } else if (a === 'o') {
+//   return "お";
+// }
 
-
-
-
-
-
-// kana if statement
-
-function detectBoin(key) {
-  if (key === 'a') {
-    return "あ";
-  } else if (key === 'i') {
-    return "い";
-  } else if (key === 'u') {
-    return "う";
-  } else if (key === 'e') {
-    return "え";
-  } else if (key === 'o') {
-    return "お";
-  } else {
-    return key
-  }
-};
-function secondKeyOfK(event) {
-  let secondKey = event.key;
-  if (secondKey === 'a') {
-    return "か";
-  } else if (secondKey === 'i') {
-    return "き";
-  } else if (secondKey === 'u') {
-    return "く";
-  } else if (secondKey === 'e') {
-    return "け";
-  } else if (secondKey === 'o') {
-    return "こ";
-  } else {
-    return X
-  }
-};
-function secondKeyOfS(event) {
-  let secondKey = event.key;
-  if (secondKey === 'a') {
-    return "さ";
-  } else if (secondKey === 'i') {
-    return "し";
-  } else if (secondKey === 'u') {
-    return "す";
-  } else if (secondKey === 'e') {
-    return "せ";
-  } else if (secondKey === 'o') {
-    return "そ";
-  }
-};
-function secondKeyOfT(event) {
-  let secondKey = event.key;
-  if (secondKey === 'a') {
-    return "た";
-  } else if (secondKey === 'i') {
-    return "ち";
-  } else if (secondKey === 'u') {
-    return "つ";
-  } else if (secondKey === 'e') {
-    return "て";
-  } else if (secondKey === 'o') {
-    return "と";
-  }
-};
-function secondKeyOfN(event) {
-  const secondKey = event.key;
-  if (secondKey === 'a') {
-    return "な";
-  } else if (secondKey === 'i') {
-    return "に";
-  } else if (secondKey === 'u') {
-    return "ぬ";
-  } else if (secondKey === 'e') {
-    return "ね";
-  } else if (secondKey === 'o') {
-    return "の";
-  }
-};
+// (key !== ('a' || 'i' || 'u' || 'e' || 'o')) {
+//   throw;
+// }
